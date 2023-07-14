fea35e840cd8a1027187283fe5b1fbffa0d57d38
fauziihsan <fauzyihsan@gmail.com>
2023-07-14 14:59:34 +0700

    app-say-hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/fauziihsan/app-say-hello
+
+go 1.18
+
+require github.com/fauziihsan/go-module-name/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	go_module_name "github.com/fauziihsan/go-module-name/v2"
+)
+
+func main() {
+	fmt.Println(go_module_name.SayName("Fauzi"))
+}
